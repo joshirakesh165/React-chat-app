85baaa2d7ec21a6cccd05f0bd6ea4aa8b49fefde
joshirakesh165 <joshirakesh165@gmail.com>
2023-12-10 19:50:10 +0530

    Create server.js

---
+++ app/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import createDBConnection from './config/db.js';
+import bodyParser from 'body-parser';
+
+import userRouter from './users/routes/user.routes.js';
+
+import { createServer } from "http";
+import cors from 'cors';
+const app = express();
+const server = createServer(app);
+import dotenv from 'dotenv';
+import chatRouter from './chat/routes/chat.routes.js';
+import configureSocket from './socket.js';
+import CustomError from './util/error.js';
+dotenv.config();
+var corsOptions = {
+    origin: process.env.ALLOWED_UI_URL
+  }
+app.use(cors(corsOptions))
+
+const SERVICE_PORT = process.env.SERVICE_PORT || 8000
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+const makeConnectionsReady = async () => {
+    try {
+        await createDBConnection();
+        configureSocket(server)
+    } catch (e) {
+        let msg = e && e.message || "Not able to stablish connection with socket or db"
+        let connectError = new Error(msg)
+        return new CustomError(connectError)
+    }
+    
+}
+
+makeConnectionsReady();
+server.listen(SERVICE_PORT,() => {
+    console.log('server running on ' , SERVICE_PORT)
+});
+
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/chat",chatRouter);
+
